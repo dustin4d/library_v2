90ddb2e125d8357e3ad0a3ce7a1d785c979100bc
unknown <Dustin@DHolder-MSI.JemisonDemsey.com>
2023-01-17 22:39:31 -0600

    Init commit :)

---
+++ app.js
@@ -0,0 +1,21 @@
+
+
+let myLibrary = []
+
+// constructor
+function Book(title, pages, hasRead) {
+    this._title = title
+    this._pages = pages
+    this._hasRead = hasRead
+}
+
+
+function addBookToLibrary() {
+    // loop through array
+    for (let i = 0; i < myLibrary.length; i++) {
+        console.log(`${i}`)
+        // TODO; 
+    }
+}
+
+// BUTTON
