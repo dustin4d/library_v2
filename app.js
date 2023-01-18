20885765deb6b470a4f3e3e2b8015a0c20199764
dustin4d <dustinholder14@yahoo.com>
2023-01-18 15:03:34 -0600

    Modal

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+const addBtn = document.querySelector(".addBtn")
+const modal = document.querySelector(".modal")
+const modalClose = document.querySelector(".modal-close")
 
-
-let myLibrary = []
+const myLibrary = []
 
 // constructor
 function Book(title, pages, hasRead) {
@@ -9,13 +11,20 @@ function Book(title, pages, hasRead) {
     this._hasRead = hasRead
 }
 
-
 function addBookToLibrary() {
     // loop through array
     for (let i = 0; i < myLibrary.length; i++) {
-        console.log(`${i}`)
-        // TODO; 
+        console.log(`${i.title}`)
     }
 }
 
 // BUTTON
+addBtn.addEventListener("click", () => {
+    console.log("Event listener called.")
+    // Open modal
+    modal.style.display = "block"
+})
+
+modalClose.addEventListener("click", () => {
+    modal.style.display = "none"
+})
